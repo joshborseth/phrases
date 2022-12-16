84f8b564a3959236b587f6cb94a50f82e1e1a5c4
Joshua Borseth <joshuaborseth@gmail.com>
2022-12-16 15:48:28 -0800

    daisy ui installed and theme setup

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,5 +4,22 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#3ABFF8",
+          secondary: "#828DF8",
+          accent: "#F471B5",
+          neutral: "#1D283A",
+          "base-100": "#0F1729",
+          info: "#0CA6E9",
+          success: "#2BD4BD",
+          warning: "#F4C152",
+          error: "#FB6F84",
+        },
+      },
+    ],
+  },
 };
