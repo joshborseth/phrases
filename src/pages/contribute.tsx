26d0594f41352bf2c0b49cd33f61640573315745
Joshua Borseth <joshuaborseth@gmail.com>
2022-12-16 16:43:11 -0800

    made some ui changes and fixed issues with fetching phrases

--- src/pages/contribute.tsx
+++ src/pages/contribute.tsx
@@ -4,7 +4,7 @@ import { type FormEvent, useRef, useState } from "react";
 import { trpc } from "../utils/trpc";
 const Contribute = () => {
   const [isDone, setIsDone] = useState(false);
-  const inputRef = useRef<HTMLTextAreaElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
   const postPhrase = trpc.phrase.createPhrase.useMutation({
     onSuccess: () => {
       if (inputRef.current?.value) inputRef.current.value = "";
@@ -32,34 +32,41 @@ const Contribute = () => {
       <Head>
         <title>Random Phrases | Contribute</title>
       </Head>
-      <form
-        className="flex min-h-screen flex-col items-center justify-center gap-20 text-center text-3xl"
-        onSubmit={(e) => handleSubmit(e)}
-      >
-        <label htmlFor="phrase">Phrase:</label>
-        <textarea
-          placeholder="Type a Phrase..."
-          ref={inputRef}
-          name="phrase"
-          className="h-80 resize-none p-2 text-black"
-        />
-        <div className="flex gap-5">
-          <button className="rounded-xl border-2 px-4 py-2" type="submit">
-            Submit
-          </button>
-          <Link href="/" className="rounded-xl border-2 px-4 py-2">
-            Back
-          </Link>
-        </div>
-        <Toast isSuccess={postPhrase.isSuccess} isDone={isDone} />
-      </form>
+      <main className="flex min-h-screen flex-col items-center justify-center">
+        <h1 className="py-20 text-4xl">Contribute a Phrase</h1>
+        <form
+          className="flex flex-col items-center justify-center gap-20"
+          onSubmit={(e) => handleSubmit(e)}
+        >
+          <div>
+            <label htmlFor="phrase" className="m-4 text-lg">
+              Phrase:
+            </label>
+            <input
+              placeholder="Type a Phrase..."
+              ref={inputRef}
+              name="phrase"
+              className="input-bordered input resize-none"
+            />
+          </div>
+          <div className="flex gap-2">
+            <button className="btn-primary btn" type="submit">
+              Submit
+            </button>
+            <Link href="/" className="btn-secondary btn">
+              Back
+            </Link>
+          </div>
+        </form>
+        <Alert isSuccess={postPhrase.isSuccess} isDone={isDone} />
+      </main>
     </>
   );
 };
 
 export default Contribute;
 
-const Toast = ({
+const Alert = ({
   isDone,
   isSuccess,
 }: {
@@ -68,11 +75,45 @@ const Toast = ({
 }) => {
   return (
     <div
-      className={`absolute top-5 right-5 rounded p-5 text-xl ${
-        isSuccess ? "bg-green-500" : "bg-red-500"
+      className={`alert mx-auto my-10 w-2/3 text-base shadow-lg ${
+        isSuccess ? "alert-success" : "alert-error"
       } ${isDone ? "opacity-100" : "opacity-0"} transition-opacity`}
     >
-      {isSuccess ? "Phrase Submitted!" : "Error Submitting Phrase"}
+      <div>
+        {isSuccess ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 flex-shrink-0 stroke-current"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 flex-shrink-0 stroke-current"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        )}
+
+        <span>
+          {isSuccess ? "Phrase Submitted!" : "Error Submitting Phrase"}
+        </span>
+      </div>
     </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,35 +4,30 @@ import Link from "next/link";
 import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
-  const { data, error, isLoading, refetch, isRefetching } =
-    trpc.phrase.getRandomPhrase.useQuery();
+  const { data, refetch, isRefetching, isFetching, isLoading } =
+    trpc.phrase.getRandomPhrase.useQuery(undefined, {
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+    });
+  const isFetchingPhrase = isFetching || isLoading || isRefetching;
   return (
     <>
       <Head>
         <title>Random Phrases | Home</title>
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center gap-20 py-20 text-center">
+      <main className="flex min-h-screen flex-col items-center justify-center gap-20 text-center text-2xl">
         <h1 className="text-4xl">Random Phrases</h1>
-        {data && !isLoading && !isRefetching && (
-          <p className="text-xl">{data.phrase}</p>
-        )}
-        {isLoading || (isRefetching && <p className="text-xl">Loading...</p>)}
-        {error && (
-          <p className="text-xl text-red-500">
-            An error occurred while fetching a phrase. {error.message}
-          </p>
-        )}
-        <button
-          onClick={() => {
-            refetch();
-          }}
-          className="rounded-xl border-2 px-4 py-2 text-2xl transition-transform hover:scale-105"
-        >
-          Generate Phrase
-        </button>
-        <Link href="/contribute" className="absolute top-5 left-5">
-          Contribute a Phrase
-        </Link>
+        <p className="text-lg">
+          {data && !isFetchingPhrase ? data.phrase : "Loading..."}
+        </p>
+        <div className="flex gap-2">
+          <Link href="/contribute" className="btn-accent btn">
+            Contribute a Phrase
+          </Link>
+          <button onClick={() => refetch()} className="btn-secondary btn">
+            Generate Phrase
+          </button>
+        </div>
       </main>
     </>
   );
