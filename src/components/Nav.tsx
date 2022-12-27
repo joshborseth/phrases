d4abfd454941b729e1bf31ab7d2a77d0dc19b944
joshborseth <joshuaborseth@gmail.com>
2022-12-27 14:45:05 -0800

    infinite scrolling added, navbar ui changes

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,27 +2,52 @@ import Link from "next/link";
 
 const Nav = () => {
   return (
-    <footer className="fixed bottom-0 left-0 right-0 w-full bg-neutral p-10">
-      <nav className="w-full">
-        <ul className="flex w-full flex-wrap justify-center gap-2">
-          <li>
-            <Link className="btn-primary btn" href="/">
-              Phrase Generator
-            </Link>
-          </li>
-          <li>
-            <Link className="btn-secondary btn" href="/contribute">
-              Contribute a Phrase
-            </Link>
-          </li>
-          <li>
-            <Link href="/manage" className="btn-accent btn">
-              Manage Phrases
-            </Link>
-          </li>
-        </ul>
+    <header>
+      <nav className="navbar fixed top-0 left-0 right-0 border-b bg-base-300 p-5 text-xl">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn-ghost btn lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="dropdown-content menu rounded-box menu-compact mt-3 w-52 border-2 bg-base-100 p-2 font-bold shadow"
+            >
+              <li>
+                <Link href="/contribute">Contribute</Link>
+              </li>
+              <li>
+                <Link href="/manage">Manage</Link>
+              </li>
+            </ul>
+          </div>
+          <Link href="/" className="btn-ghost btn text-2xl normal-case lg:text-3xl">
+            Phrase Generator
+          </Link>
+          <ul className="menu menu-horizontal hidden px-1 lg:flex">
+            <li>
+              <Link href="/contribute">Contribute</Link>
+            </li>
+            <li>
+              <Link href="/manage">Manage</Link>
+            </li>
+          </ul>
+        </div>
       </nav>
-    </footer>
+    </header>
   );
 };
 
--- src/pages/manage.tsx
+++ src/pages/manage.tsx
@@ -1,58 +1,56 @@
 import { Phrase } from "@prisma/client";
 import { type NextPage } from "next";
 import Head from "next/head";
-import { useState } from "react";
+import Image from "next/image";
 import Nav from "../components/Nav";
 import { trpc } from "../utils/trpc";
 
 const Manage: NextPage = () => {
-  const [page, setPage] = useState(0);
-  const { data, fetchNextPage, error, isLoading, isFetchingNextPage } =
+  const { data, fetchNextPage, hasNextPage, error, isLoading, isFetchingNextPage } =
     trpc.phrase.getAllPhrases.useInfiniteQuery(
       {
-        limit: 4,
+        limit: 8,
       },
       {
         getNextPageParam: (lastPage) => lastPage.nextCursor,
       }
     );
-  const handleFetchNextPage = () => {
-    fetchNextPage();
-    setPage((prev) => prev + 1);
-  };
-
-  const handleFetchPreviousPage = () => {
-    setPage((prev) => prev - 1);
-  };
-
   // data will be split in pages
-  const toShow = data?.pages[page]?.items;
   const loading = isLoading || isFetchingNextPage;
   return (
-    <div className="flex min-h-screen flex-col justify-start pb-80 md:pb-0">
+    <>
+      <Nav />
       <Head>
         <title>Random Phrases | Manage Phrases</title>
       </Head>
-      <main className="text-center text-lg">
-        <h1 className="py-10 text-4xl">Manage Phrases</h1>
-        <section className="grid grid-cols-1 sm:grid-cols-2">
-          {toShow?.map((phrase) => (
-            <Phrase key={phrase.id} phrase={phrase} />
-          ))}
+      <main className="flex min-h-screen flex-col justify-center text-center text-lg">
+        <h1 className="pt-32 pb-10 text-4xl">Manage Phrases</h1>
+        <section className="grid grid-cols-1 py-10 sm:grid-cols-2">
+          {data?.pages?.map((page) =>
+            page.items.map((phrase) => <Phrase key={phrase.id} phrase={phrase} />)
+          )}
           {error && <p className="col-span-3">{error.message}</p>}
-          {loading && <p className="col-span-3">Loading...</p>}
+          {loading && (
+            <div className="flex w-screen justify-center">
+              <Image
+                height={100}
+                width={100}
+                alt="Loading Spinner"
+                src="/ball-triangle.svg"
+              />
+            </div>
+          )}
         </section>
-        <div className="flex w-full justify-center gap-5">
-          <button onClick={() => handleFetchPreviousPage()} className="btn-warning btn">
-            Previous
+        {hasNextPage && (
+          <button
+            onClick={() => fetchNextPage()}
+            className="btn-secondary btn mx-auto my-10"
+          >
+            Load More
           </button>
-          <button onClick={() => handleFetchNextPage()} className="btn-success btn">
-            Next
-          </button>
-        </div>
+        )}
       </main>
-      <Nav />
-    </div>
+    </>
   );
 };
 
@@ -66,7 +64,7 @@ const Phrase = ({ phrase }: { phrase: Phrase }) => {
     },
   });
   return (
-    <article className="m-10 flex items-center justify-between gap-5 rounded border-2 p-4 text-left">
+    <article className="mx-5 my-2 flex items-center justify-between gap-5 rounded border-2 p-4 text-left lg:mx-16">
       <p>{phrase.phrase}</p>
       <button className="btn-error btn h-10 w-10" onClick={() => mutate(phrase.id)}>
         X
