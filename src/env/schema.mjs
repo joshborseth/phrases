cd6554c2d8fbbeaaefcd32ac36f577696db6549a
Joshua Borseth <joshuaborseth@gmail.com>
2022-12-31 14:02:53 -0800

    starting to add auth, next auth installed and types fixed

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -8,6 +8,8 @@ import { z } from "zod";
 export const serverSchema = z.object({
   DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
+  DISCORD_CLIENT_ID: z.string(),
+  DISCORD_CLIENT_SECRET: z.string(),
 });
 
 /**
