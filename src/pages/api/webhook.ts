b531f3a79b5dfc225e9a198af795f0903c33cc8d
joshborseth <joshuaborseth@gmail.com>
2023-02-04 18:06:33 -0800

    please be slaying

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -2,7 +2,7 @@ import { buffer } from "micro";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const rawBody = (await buffer(req)).toString();
+  const rawBody = await buffer(req);
   return res.status(200).json({ rawBody });
 };
 
