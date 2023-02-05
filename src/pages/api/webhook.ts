997b08c7a57cc5974491d9db643bb9103a5606a4
joshborseth <joshuaborseth@gmail.com>
2023-02-04 16:36:56 -0800

    to each their own

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -3,5 +3,6 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const rawBody = await buffer(req);
+  console.log("each");
   res.status(200).json(rawBody);
 };
