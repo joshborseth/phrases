405d211bea9708d3a5fcdb5f4320be8c11cea025
joshborseth <joshuaborseth@gmail.com>
2023-02-04 17:00:12 -0800

    to each their own

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -2,8 +2,10 @@ import { buffer } from "micro";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const rawBody = await buffer(req);
-  res.status(200).json(rawBody);
+  if (req.method === "POST") {
+    const buf = await buffer(req);
+    res.status(200).json(buf);
+  }
 };
 
 export const config = {
