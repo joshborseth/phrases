cbc2e87ff9e2504b7f0c48b3c564ad62ed49462e
joshborseth <joshuaborseth@gmail.com>
2023-02-04 16:32:53 -0800

    learning webhooks lol again

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -2,6 +2,6 @@ import { buffer } from "micro";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const rawBody = (await buffer(req)).toString();
-  return rawBody;
+  const rawBody = await buffer(req);
+  res.status(200).json(rawBody);
 };
