44f6c6234dc6ed34df2ca0f9bbf8ac9a4e999043
joshborseth <joshuaborseth@gmail.com>
2023-02-04 16:44:29 -0800

    to each their own

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -3,6 +3,6 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const rawBody = await buffer(req);
-  console.log("each");
+  console.log("each their own");
   res.status(200).json(rawBody);
 };
