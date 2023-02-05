691ef04b40acbcc9e8c7c656e3a28c4d641b42ab
joshborseth <joshuaborseth@gmail.com>
2023-02-04 17:34:13 -0800

    still trying web hoooks

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -1,18 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import multiparty from "multiparty";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const form = new multiparty.Form();
-  const data = await new Promise((resolve, reject) => {
-    form.parse(req, function (err, fields, files) {
-      if (err) reject({ err });
-      resolve({ fields, files });
-    });
-  });
-
-  console.log(`Form data: `, data);
-
-  return res.status(200).json("hey man");
+  return res.status(200).json({ data: "Hello World" });
 };
 
 export const config = {
